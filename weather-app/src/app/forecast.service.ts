0235da7b384535c4e24332ee46c6b9281e6775a8
Francisca van maurik <franciscavanmaurik@gmail.com>
2019-05-05 18:00:11 +0200

    settings for icons

--- weather-app/src/app/forecast.service.ts
+++ weather-app/src/app/forecast.service.ts
@@ -2,12 +2,16 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 
 import { Observable } from 'rxjs';
+import * as constants from '../../src/app/constants/constants';
+
+
 
 @Injectable()
+
+
 export class ForecastService {
 
-
-   url = `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/52.025127600000005,5.070914999999999?lat=52.025127600000005&lng=5.070914999999999`;
+   url = `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/${constants.DARKSKY_API_KEY}/${lat},${lng}`;
 
   constructor(private http: HttpClient) {}
 
@@ -17,7 +21,7 @@ export class ForecastService {
     params = params.set('lat', lat.toString() )
     params = params.set('lng', lng.toString() )
     console.log(lat,lng)
-    return this.http.get(this.url, { params })
+    return this.http.get(this.url, { params:params })
   
   }
 }
